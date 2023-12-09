cef8fe3b1f1c260b481df2ffd8842b90bcb83393
Emzzy241 <emzzyoluwole@gmail.com>
2023-12-09 20:44:47 +0100

    add all UI logic for app

--- Palindromes/Models/PalindromeChecker.cs
+++ Palindromes/Models/PalindromeChecker.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Palindromes.Models
 {
@@ -13,9 +14,9 @@ namespace Palindromes.Models
 
         // Converting the string into a string array so I can reverse it
 
-        public string ConvertStrToCharArray(string theWord)
+        public string ConvertStrToCharArray()
         {
-            char[] theCharArray = theWord.ToCharArray();
+            char[] theCharArray = PalindromeWord.ToCharArray();
             // char[]  results = theCharArray.Reverse();
 
             Array.Reverse(theCharArray);
@@ -25,19 +26,41 @@ namespace Palindromes.Models
 
             
         }
+        
+        // Writing the method without a .Reverse().... The method has no Array.Reverse() in it 
+        // public static bool CheckPalindrome(string words)
+        // {
+        //     char[] myChar = words.ToCharArray();
+        //     string reversedString = "";
+
+        //     // A forloop
+        //     for (int i = myChar.Length-1; i > -1; i--)
+        //     {
+        //         // first: where to start from, go to the last element... Since Length counts like: 12345 instead of counting with index 01234
+        //         // Stop when i > -1
+        //         // how to move: move backwards
+        //         reversedString += myChar[i];
+
+        //     }
+        //     // The code below is a short way of using an if statement to determine if two values are equal....
+        //     // The code below returns a boolean(true or false)
+        //     return words == reversedString;
+        // }
 
         // Determiner
         public string ResultDeterminer()
         {
-        if (ConvertStrToCharArray() == word)
-        {
-            return "Your word is a palindrome";
-        }
-        else
-        {
-            return "Your word is not a Palindrome";
-        }
+            if (ConvertStrToCharArray() == PalindromeWord)
+            {
+                return "Your word is a palindrome";
+            }
+            else
+            {
+                return "Your word is not a Palindrome";
+            }
         }   
+
+
 
 
         
--- Palindromes/Program.cs
+++ Palindromes/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using Palindromes.Models;
+using System.Collections.Generic;
 
 namespace Palindromes.Models
 {
@@ -7,7 +8,51 @@ namespace Palindromes.Models
     {
         static void Main()
         {
-            Console.WriteLine("Hola");
+            Console.WriteLine();
+            Console.WriteLine("Palindrome Checker");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Enter your word: ");
+            string inputWord = Console.ReadLine().ToLower();
+            Console.WriteLine();
+            PalindromeChecker Word = new PalindromeChecker(inputWord);
+            Console.WriteLine();
+            Console.WriteLine("Your Word backwards: " + Word.ConvertStrToCharArray());
+            Console.WriteLine(Word.ResultDeterminer());
+
+            Console.WriteLine("To enter another word, enter 'y' for yes or 'n'");
+            string userContinue = Console.ReadLine().ToLower();
+            Console.WriteLine();
+
+            if(userContinue == "y")
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Main();
+            }
+            else if (userContinue == "n")
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Goodbye");
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Sorry, I didn't get that. Lets try again");
+                Main();
+            }
+
+            // The way of the note, using only one static method
+            // To use, uncomment out its method which is the CHeckPalindrome() method in business logic file
+
+            // Console.WriteLine("Palindrome Checker");
+            // Console.WriteLine();
+            // Console.WriteLine("Input a word to check if it is a palindrome");
+            // string  word = Console.ReadLine().ToLower();
+            // Console.WriteLine(PalindromeChecker.CheckPalindrome(word));
+
         }
     }
 }
